0bc409026d5c9e299381c225d1ffae8c32bf6d2d
Bear Giles <bgiles@coyotesong.com>
2015-07-16 12:58:05 -0600

    Refined SQL and C.

--- sql/pg_complex.sql
+++ sql/pg_complex.sql
@@ -9,63 +9,65 @@
 CREATE TYPE complex AS (re float8, im float8);
 
 --
--- create functions
+-- create functions implemented in SQL.
 --
-CREATE OR REPLACE FUNCTION float8_as_complex(float8)
-RETURNS complex
-AS 'pg_complex_from_float8'
-LANGUAGE C IMMUTABLE STRICT;
+CREATE OR REPLACE FUNCTION pgx_complex_from_float8(float8) RETURNS complex AS $$
+   SELECT ROW($1, 0)::complex;
+$$ LANGUAGE SQL IMMUTABLE STRICT;
 
-CREATE OR REPLACE FUNCTION pg_eq(complex, complex)
+CREATE OR REPLACE FUNCTION pgx_complex_negate(complex) RETURNS complex AS $$
+   SELECT ROW(-$1.re, -$1.im)::complex;
+$$ LANGUAGE SQL IMMUTABLE STRICT;
+
+CREATE OR REPLACE FUNCTION pgx_complex_conjugate(complex) RETURNS complex AS $$
+   SELECT ROW($1.re, -$1.im)::complex;
+$$ LANGUAGE SQL IMMUTABLE STRICT;
+
+CREATE OR REPLACE FUNCTION pgx_complex_add(complex, complex) RETURNS complex AS $$
+   SELECT ROW($1.re + $2.re, $1.im + $2.im)::complex;
+$$ LANGUAGE SQL IMMUTABLE STRICT;
+
+CREATE OR REPLACE FUNCTION pgx_complex_sub(complex, complex) RETURNS complex AS $$
+   SELECT ROW($1.re - $2.re, $1.im - $2.im)::complex;
+$$ LANGUAGE SQL IMMUTABLE STRICT;
+
+CREATE OR REPLACE FUNCTION pgx_complex_multiply(complex, complex) RETURNS complex AS $$
+   SELECT ROW($1.re * $2.re - $1.im * $2.im, $1.re * $2.im + $1.im * $2.re)::complex;
+$$ LANGUAGE SQL IMMUTABLE STRICT;
+
+--
+-- create functions implemented in C.
+--
+CREATE OR REPLACE FUNCTION eq(complex, complex)
 RETURNS bool
-AS 'pg_complex_eq'
+AS 'pgx_complex_eq'
 LANGUAGE C IMMUTABLE STRICT;
 
-CREATE OR REPLACE FUNCTION pg_negate(complex)
+CREATE OR REPLACE FUNCTION near(complex, complex)
+RETURNS bool
+AS 'pgx_complex_near'
+LANGUAGE C IMMUTABLE STRICT;
+
+CREATE OR REPLACE FUNCTION divide(complex, complex)
 RETURNS complex
-AS 'pg_complex_negate'
+AS 'pgx_complex_divide'
 LANGUAGE C IMMUTABLE STRICT;
 
-CREATE OR REPLACE FUNCTION pg_conjugate(complex)
-RETURNS complex
-AS 'pg_complex_conjugate'
-LANGUAGE C IMMUTABLE STRICT;
-
-CREATE OR REPLACE FUNCTION pg_add(complex, complex)
-RETURNS complex
-AS 'pg_complex_add_cc'
-LANGUAGE C IMMUTABLE STRICT;
-
-CREATE OR REPLACE FUNCTION pg_subtract(complex, complex)
-RETURNS complex
-AS 'pg_complex_subtract'
-LANGUAGE C IMMUTABLE STRICT;
-
-CREATE OR REPLACE FUNCTION pg_multiply(complex, complex)
-RETURNS complex
-AS 'pg_complex_multiply'
-LANGUAGE C IMMUTABLE STRICT;
-
-CREATE OR REPLACE FUNCTION pg_divide(complex, float8)
-RETURNS complex
-AS 'pg_complex_divide'
-LANGUAGE C IMMUTABLE STRICT;
-
-CREATE OR REPLACE FUNCTION pg_magnitude(complex)
+CREATE OR REPLACE FUNCTION magnitude(complex)
 RETURNS float8
-AS 'pg_complex_magnitude'
+AS 'pgx_complex_magnitude'
 LANGUAGE C IMMUTABLE STRICT;
 
-CREATE OR REPLACE FUNCTION pg_norm(complex)
+CREATE OR REPLACE FUNCTION norm(complex)
 RETURNS complex
-AS 'pg_complex_norm'
+AS 'pgx_complex_norm'
 LANGUAGE C IMMUTABLE STRICT;
 
 --
 -- create casts
 --
 CREATE CAST (float8 AS complex)
-WITH FUNCTION pg_complex_from_float8(float8)
+WITH FUNCTION from_float8(float8)
 AS ASSIGNMENT;
 
 --
@@ -74,44 +76,50 @@ AS ASSIGNMENT;
 CREATE OPERATOR = (
    LEFT_ARG = complex,
    RIGHT_ARG = complex,
-   PROCEDURE = pg_complex_eq,
+   PROCEDURE = eq,
    NEGATOR = <>,
    HASHES,
    MERGES
 );
 
+CREATE OPERATOR ~ (
+   LEFT_ARG = complex,
+   RIGHT_ARG = complex,
+   PROCEDURE = near
+);
+
 CREATE OPERATOR - (
    RIGHT_ARG = complex,
-   PROCEDURE = pg_complex_negate,
+   PROCEDURE = pgx_complex_negate,
    NEGATOR = -
 );
 
 CREATE OPERATOR ~ (
    RIGHT_ARG = complex,
-   PROCEDURE = pg_complex_conjugate,
+   PROCEDURE = pgx_complex_conjugate,
    NEGATOR = ~
 );
 
 CREATE OPERATOR + (
    LEFT_ARG = complex,
    RIGHT_ARG = complex,
-   PROCEDURE = pg_complex_add
+   PROCEDURE = pgx_complex_add
 );
 
 CREATE OPERATOR - (
    LEFT_ARG = complex,
    RIGHT_ARG = complex,
-   PROCEDURE = pg_complex_subtract
+   PROCEDURE = pgx_complex_subtract
 );
 
 CREATE OPERATOR * (
    LEFT_ARG = complex,
    RIGHT_ARG = complex,
-   PROCEDURE = pg_complex_multiply
+   PROCEDURE = pgx_complex_multiply
 );
 
 CREATE OPERATOR / (
    LEFT_ARG = complex,
    RIGHT_ARG = float8,
-   PROCEDURE = pg_complex_divide
+   PROCEDURE = divide
 );
--- src/pg_complex.c
+++ src/pg_complex.c
@@ -4,102 +4,54 @@
 PG_MODULE_MAGIC;
 
 /*
- *
+ * Test complex numbers for equality. Note: this has all of the usual problems with
+ * testing floats and doubles for equality. Consider using ~= (near) instead.
  */
-PG_FUNCTION_INFO_V1(pg_complex_from_float8);
+PG_FUNCTION_INFO_V1(pgx_complex_eq);
 
 Datum
-pg_complex_from_float8(PG_FUNCTION_ARGS) {
+pgx_complex_eq(PG_FUNCTION_ARGS) {
+	PG_RETURN_NULL();
+}
+
+/*
+ * Test complex numbers for proximity. This avoids the problems with testing floats
+ * and doubles but does not guarantee absolute equality.
+ */
+PG_FUNCTION_INFO_V1(pgx_complex_near);
+
+Datum
+pgx_complex_near(PG_FUNCTION_ARGS) {
+    PG_RETURN_NULL();
+}
+
+/*
+ *
+ */
+PG_FUNCTION_INFO_V1(pgx_complex_divide);
+
+Datum
+pgx_complex_divide(PG_FUNCTION_ARGS) {
 	PG_RETURN_NULL();
 }
 
 /*
  *
  */
-PG_FUNCTION_INFO_V1(pg_complex_eq);
+PG_FUNCTION_INFO_V1(pgx_complex_magnitude);
 
 Datum
-pg_complex_eq(PG_FUNCTION_ARGS) {
+pgx_complex_magnitude(PG_FUNCTION_ARGS) {
 	PG_RETURN_NULL();
 }
 
 /*
  *
  */
-PG_FUNCTION_INFO_V1(pg_complex_negate);
+PG_FUNCTION_INFO_V1(pgx_complex_norm);
 
 Datum
-pg_complex_negate(PG_FUNCTION_ARGS) {
-	PG_RETURN_NULL();
-}
-
-/*
- *
- */
-PG_FUNCTION_INFO_V1(pg_complex_conjugate);
-
-Datum
-pg_complex_conjugate(PG_FUNCTION_ARGS) {
-	PG_RETURN_NULL();
-}
-
-/*
- *
- */
-PG_FUNCTION_INFO_V1(pg_complex_add);
-
-Datum
-pg_complex_add(PG_FUNCTION_ARGS) {
-	PG_RETURN_NULL();
-}
-
-/*
- *
- */
-PG_FUNCTION_INFO_V1(pg_complex_subtract);
-
-Datum
-pg_complex_subtract(PG_FUNCTION_ARGS) {
-	PG_RETURN_NULL();
-}
-
-/*
- *
- */
-PG_FUNCTION_INFO_V1(pg_complex_multiply);
-
-Datum
-pg_complex_multiply(PG_FUNCTION_ARGS) {
-	PG_RETURN_NULL();
-}
-
-/*
- *
- */
-PG_FUNCTION_INFO_V1(pg_complex_divide_cf);
-
-Datum
-pg_complex_divide_cf(PG_FUNCTION_ARGS) {
-	PG_RETURN_NULL();
-}
-
-/*
- *
- */
-PG_FUNCTION_INFO_V1(pg_complex_magnitude);
-
-Datum
-pg_complex_magnitude(PG_FUNCTION_ARGS) {
-	PG_RETURN_NULL();
-}
-
-/*
- *
- */
-PG_FUNCTION_INFO_V1(pg_complex_norm);
-
-Datum
-pg_complex_norm(PG_FUNCTION_ARGS) {
+pgx_complex_norm(PG_FUNCTION_ARGS) {
 	PG_RETURN_NULL();
 }
 
