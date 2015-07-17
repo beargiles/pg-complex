a16448ed43a8b037063db7bd04b47031ddc2258e
Bear Giles <bgiles@coyotesong.com>
2015-07-17 08:41:13 -0600

    Updated tests.

--- test/sql/math.sql
+++ test/sql/math.sql
@@ -3,50 +3,31 @@ BEGIN;
 \i sql/pg_complex.sql
 \set ECHO all
 
+\set c1 (1,2)::complex
+\set c2 (1,1)::complex
+\set c3 (3,4)::complex
+\set c4 (3,8)::complex
+
 select 1::complex as a, (1::int8)::complex as b, 1.0::complex as c;
 
 select (1,2)::complex as a, -(1,2)::complex as b, ~(1,2)::complex as c;
 
-select (1,2)::complex + (3,4)::complex as a,
-  3 + (1,2)::complex as b, (1,2)::complex + 3 as c;
+select :c1 + (3,4)::complex as a, 3 + :c1 as b, :c1 + 3 as c;
 
-select (1,2)::complex - (3,6)::complex as a,
-  3 - (1,2)::complex as b, (1,2)::complex - 3 as c;
+select :c1 - (3,6)::complex as a, 3 - :c1 as b, :c1 - 3 as c;
 
-select (1,2)::complex * (3,5)::complex as a,
-  3 * (1,2)::complex as b, (1,2)::complex * 3 as c;
+select :c1 * (3,5)::complex as a, 3 * :c1 as b, :c1 * 3 as c;
+select :c1 * (3,5)::complex as a, 3.0::double precision * :c1 as b, :c1 * 3.0 as c;
 
-select (3,8)::complex / (1,2)::complex as c;
-
-select magnitude((1, 1)::complex) as magnitude;
+select :c4 / :c1  as a, (3.8, 0.4)::complex * :c1 = :c4 as b;
+select :c4 / (2,0)::complex as c;
+select :c4 / (0,2)::complex as c;
 
 --
--- check norm.
+-- check magnitude
 --
-select norm((1,0)::complex) as norm;
-
-select norm((0,1)::complex) as norm;
-
-select norm((-1, 0)::complex) as norm;
-
-select norm((0, -1)::complex) as norm;
-
-select norm((0, 0)::complex) as norm;
-
-select magnitude(norm((1, 1)::complex)) as magnitude;
-
---
--- test equality
---
-select (1,2)::complex = (1,2)::complex as b;
-
-select (1,2)::complex = (2,1)::complex as b;
-
---
--- test proximity
---
-select (1,2)::complex ~ (1,2)::complex as b;
-
-select (1,2)::complex ~ (2,1)::complex as b;
+select magnitude(:c1) as magnitude;
+select magnitude(:c2) as magnitude;
+select magnitude(:c3) as magnitude;
 
 ROLLBACK;
